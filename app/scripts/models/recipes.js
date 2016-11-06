99a3adc4f9fa5d7845dcc3229afdc79c7c58124f
Matias Mariani <matiasironyard@gmail.com>
2016-11-06 17:12:26 -0500

    stuck

--- app/scripts/models/recipes.js
+++ app/scripts/models/recipes.js
@@ -3,6 +3,24 @@ var Backbone = require('backbone');
 var Recipe = Backbone.Model.extend({
   idAttribute: 'objectId',
   //set up recipe: defaults and methods to update servings/ingredient amount.
+  defaults: {
+    title: "Your Recipe Title",
+    qty: 1,
+    qtyMeasurement: 'imperial',
+    ingredients: []
+  },
+  ConversionFactor: function(newQty){
+    //pass in newQty param from AdjustRecipeContainer...
+    var orgQty = this.get('qty');
+    this.updatedQtys(newQty/orgQty);
+    //set the ConversionFactor by taking the qty(orgQty) and setting it to newQty
+    this.set('qty', newQty)
+  },
+
+ updateQtys: function(){
+   //
+ },
+
 
 });
 
